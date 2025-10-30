1f03c38f50d0e784c682e6c0a03da4a2e7301129
MarioRamos0 <marioandresramosh@gmail.com>
2025-10-30 14:30:31 -0400

    aa

--- main.py
+++ main.py
@@ -35,4 +35,4 @@ app.include_router(users_router, prefix="/users", tags=["Users"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=9095, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
